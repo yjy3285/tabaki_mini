ff86fcd3e45bac6d1544f335655b4f81760f8f4d
amerareck <tjdgus510@gmail.com>
2024-08-14 18:35:13 +0900

    update: 주문정보 수정버튼 js 추가
    form 태그의 기능을 막고, 수정하려는 정보를 출력하는 식으로 일단 구현했습니다.
    나중에 ajax로 처리해야 합니다.

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -49,3 +49,48 @@ $('#orderSearchForm').submit(function(event){
         alert('검색을 진행합니다.\n선택한 검색 타입 : '+selector.val());
     }
 });
+
+function execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            var addr = '';
+            var extraAddr = '';
+
+            // 도로명 주소와 지번 주소 구분
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+            
+            // 주소 필드에 값 설정
+            document.getElementById('recevierPostNo').value = data.zonecode;
+            document.getElementById('recevierAddr').value = addr;
+            document.getElementById('recevierDetailsAddr').focus();
+        }
+    }).open();
+}
+
+$('#orderUpdateSubmit').submit(function(event){
+    event.preventDefault();
+    
+    const phoneNumber = $('#receiverPhone').val();
+    const orderProductName = [];
+    const orderProductState = [];
+    const orderProductData = $('#orderProductData');
+    orderProductData.each(function(index){
+        orderProductData.push($(this).find('td div.product-names').val());
+        orderProductState.push($(this).find('select.form-control option:selected').val());
+    });
+    phoneNumber.replace('\[^0-9]\g', '');
+    let str = '수정을 하려고 하는 내역은 다음과 같습니다.\n';
+    str += '이름 : '+ $('#receiver').val()+'\n';
+    str += '폰번호 : '+ $('#receiverPhone').val()+'\n';
+    str += '우편번호 : '+ $('#recevierPostNo').val()+'\n';
+    str += '주소 : '+ $('#recevierAddr').val()+' '+$('#recevierDetailsAddr').val()+'\n';
+    str += orderProductData.length;
+    for(let i=0; i<orderProductData.length; i++) {
+        str+= '배송상태 : '+orderProductName[i]+' - '+orderProductState[i];
+    }
+    alert(str);
+});
